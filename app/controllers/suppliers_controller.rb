09775ed28f87310848dfe34b0b432f32cfe15fa1
Camden <camdencparker@gmail.com>
2022-01-25 08:33:43 -0700

    add index and create methods to supplier controller

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -1,2 +1,19 @@
 class SuppliersController < ApplicationController
+  def index
+    suppliers = Supplier.all
+    render json: suppliers
+  end
+
+  def create
+    supplier = Supplier.new(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    if supplier.save
+      render json: supplier
+    else
+      render json: {errors: supplier.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
 end
